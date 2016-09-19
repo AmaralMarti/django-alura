fa75dac05e72c2dc8296aaa5ee93f77271abd0ff
Henrique Marti <henrique@webmarti.com.br>
2016-09-19 18:11:14 -0300

    Aula 5

--- perfis/views.py
+++ perfis/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'perfis': Perfil.objects.all()})
 
 def exibir(request, perfil_id):
-
+    
     try:
-    	perfil = Perfil.objects.get(id=perfil_id)
-    	return render(request, 'perfil.html', {'perfil': perfil})
-    except Exception, e:
-    	return render(request, 'perfilinvalido.html', {'perfil_id': perfil_id})
+    	perfil = Perfil.objects.get(id=perfil_id)    	
+    except Exception:
+    	perfil = None
+
+    return render(request, 'perfil.html', {'perfil': perfil, 'perfil_id': perfil_id})	
