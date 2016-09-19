3dc217416f8e5bfd0c5c8e1f272fedcb634260b6
Henrique Marti <henrique@webmarti.com.br>
2016-09-19 16:27:32 -0300

    Aula 3

--- perfis/views.py
+++ perfis/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 def index(request):
     return render(request, 'index.html')
 
+def exibir(request, perfil_id):
+
+    perfil = Perfil()
+
+    if perfil_id == '1':
+    	perfil = Perfil('Henrique', 'henrique@henrique.com.br', '12345678', 'empresa')  
+    	return render(request, 'perfil.html', {'perfil': perfil})
+    else:
+    	return render(request, 'perfilinvalido.html', {'perfil_id': perfil_id})
