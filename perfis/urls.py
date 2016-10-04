8c7bb125314606e86829ef4abae551d3e69f973b
Ubuntu <ubuntu@ip-172-31-34-244.us-west-2.compute.internal>
2016-10-04 19:10:38 +0000

    Acertando

--- perfis/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from perfis import views 
-
-urlpatterns = patterns('',    
-    url(r'^$', views.index, name='index'),
-    url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='perfis'),
-    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar'),
-    url(r'^convite/(?P<convite_id>\d+)/aceitar$', views.aceitar, name='aceitar')
-)
